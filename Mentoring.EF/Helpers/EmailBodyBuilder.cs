84039e4e44a3284631909eae127b527de63259bf
ahmd-naser <ahmdnasr197@gmail.com>
2026-08-09 19:09:04 +0300

    Added emailBodyBuilder

---
+++ Mentoring.EF/Helpers/EmailBodyBuilder.cs
@@ -0,0 +1,18 @@
+﻿namespace Mentoring.EF.Helpers;
+
+public static class EmailBodyBuilder
+{
+    public static string GenerateEmailBody(string template , Dictionary<string, string> placeholders)
+    {
+        var templatePath = $"{Directory.GetCurrentDirectory()}/Templates/{template}.html"; 
+        var streamReader = new StreamReader(templatePath);
+        var body = streamReader.ReadToEnd();
+        streamReader.Close();
+
+        foreach(var placeholder in placeholders)
+            body = body.Replace(placeholder.Key, placeholder.Value);
+        
+
+        return body;
+    }
+}
