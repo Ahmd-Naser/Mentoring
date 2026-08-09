008d3b6dfcfcef035feea071e6e2bf2adfe1be8a
ahmd-naser <ahmdnasr197@gmail.com>
2026-08-09 18:01:58 +0300

    added enail configurations

--- Mentoring.Api/DependencyInjection.cs
+++ Mentoring.Api/DependencyInjection.cs
@@ -37,6 +37,8 @@ public static class DependencyInjection
         services.AddScoped<ISubmissionService, SubmissionService>();
         services.AddScoped<IAuthService, AuthService>();
 
+        services.Configure<MailSettings>(configuration.GetSection(nameof(MailSettings) ));
+
 
         return services;
 
